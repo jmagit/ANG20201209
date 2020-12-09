b64752dc95d061252d79fa489e7598c695c8a3a1
Javier <jmartin@grupoloyal.com>
2020-12-09 20:01:37 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cursoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cursoApp
+ */
+angular.module('cursoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
