c323e3a1f1ecb0ba3c53c8c57145829907bc5081
Javier <jmartin@grupoloyal.com>
2020-12-10 18:53:29 +0100

    filtros

--- app/scripts/my-core.js
+++ app/scripts/my-core.js
@@ -1,3 +1,11 @@
 var myCore = angular.module('myCore', []);
 
-// myCore.factory(...)
+myCore.filter("capitalize", function () {
+  return function (s) {
+    if (typeof (s) === "string") {
+      return s.charAt(0).toUpperCase() +
+        s.substring(1, s.length).toLowerCase();
+    }
+    return s;
+  };
+});
