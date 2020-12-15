999a2f6250811d89f0e97f79d8cbfb7713dc6eb3
Javier <jmartin@grupoloyal.com>
2020-12-15 19:22:16 +0100

    Fornularios

--- app/scripts/controllers/personas.js
+++ app/scripts/controllers/personas.js
@@ -1,4 +1,16 @@
 ﻿angular.module('cursoApp').controller('PersonasController',
   ['$log', '$scope', function ($log, $scope) {
     var vm = this;
+    vm.listado = [];
+    vm.elemento = { id: 1, nombre: 'Pepito', apellidos: 'Grillo', edad: 99 };
+    vm.modo = 'edit';
+    vm.elemento = {  };
+    vm.modo = 'add';
+
+    vm.cancelar = function() {
+      alert("Atras");
+    }
+    vm.enviar = function() {
+      alert(JSON.stringify(vm.elemento));
+    }
   }]);
