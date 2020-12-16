108d88bf507eed4ca3d7f17ee567ac86c49eb710
Javier <jmartin@grupoloyal.com>
2020-12-16 19:36:52 +0100

    ViewModel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,113 +19,134 @@ var cursoAppModule = angular
     'myCore'
   ]);
 cursoAppModule.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/demo', {
-        templateUrl: 'views/demo.html',
-        controller: 'DemoController',
-        controllerAs: 'vm'
-      })
-      .when('/calculadora', {
-        templateUrl: 'views/calculadora.html',
-        controller: 'CalculadoraController',
-        controllerAs: 'ctrl'
-      })
-      .when('/calc', {
-        templateUrl: 'views/calculadora.en.html',
-        controller: 'CalculadoraController',
-        controllerAs: 'ctrl'
-      })
-      .when('/personas', {
-        templateUrl: 'views/personas/formulario.html',
-        controller: 'PersonasController',
-        controllerAs: 'vm'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+    })
+    .when('/demo', {
+      templateUrl: 'views/demo.html',
+      controller: 'DemoController',
+      controllerAs: 'vm'
+    })
+    .when('/calculadora', {
+      templateUrl: 'views/calculadora.html',
+      controller: 'CalculadoraController',
+      controllerAs: 'ctrl'
+    })
+    .when('/calc', {
+      templateUrl: 'views/calculadora.en.html',
+      controller: 'CalculadoraController',
+      controllerAs: 'ctrl'
+    })
+    .when('/personas', {
+      templateUrl: 'views/personas/listado.html',
+      controller: 'PersonasController',
+      controllerAs: 'vm'
+    })
+    .when('/personas/add', {
+      templateUrl: 'views/personas/formulario.html',
+      controller: 'PersonasController',
+      controllerAs: 'vm'
+    })
+    .when('/personas/:id/edit', {
+      templateUrl: 'views/personas/formulario.html',
+      controller: 'PersonasController',
+      controllerAs: 'vm'
+    })
+    .when('/personas/:id', {
+      templateUrl: 'views/personas/detalle.html',
+      controller: 'PersonasController',
+      controllerAs: 'vm'
+    })
+    .when('/personas/:id/:kk*', {
+      templateUrl: 'views/personas/detalle.html',
+      controller: 'PersonasController',
+      controllerAs: 'vm'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
 cursoAppModule.constant('version', 1);
-cursoAppModule.factory('TitleService', [function() {
+cursoAppModule.constant('URLApi', 'http://localhost:4321/api/');
+cursoAppModule.factory('TitleService', [function () {
   var titulo = 'Curso de AngularJS';
   return {
-    Titulo: function(newName) {
+    Titulo: function (newName) {
       return arguments.length ? (titulo = newName) : titulo;
-      }
+    }
   };
 }]);
-cursoAppModule.factory('NotificationService', [function() {
+cursoAppModule.factory('NotificationService', [function () {
   var listado = [];
   return {
-    add: function(msg) {
-        var id = listado.length == 0 ? 1 : (listado[listado.length - 1].id + 1);
-        listado.push({id: id, mensaje: msg});
+    add: function (msg) {
+      var id = listado.length == 0 ? 1 : (listado[listado.length - 1].id + 1);
+      listado.push({ id: id, mensaje: msg });
     },
-    remove: function(index) {
+    remove: function (index) {
       listado.splice(index, 1);
     },
-    clear: function() {
+    clear: function () {
       listado = [];
     },
-    hayNotificaciones: function() { return listado.length > 0; },
-    notificaciones: function() { return Object.assign([], listado);}
+    hayNotificaciones: function () { return listado.length > 0; },
+    notificaciones: function () { return Object.assign([], listado); }
   };
 }]);
 
-cursoAppModule.controller('ApplicationController', ['TitleService','NotificationService', function(TitleService, notify) {
+cursoAppModule.controller('ApplicationController', ['TitleService', 'NotificationService', function (TitleService, notify) {
   var vm = this;
 
-  vm.dameTitulo = function() { return TitleService.Titulo() }
+  vm.dameTitulo = function () { return TitleService.Titulo() }
   vm.Notify = notify;
 }]);
-cursoAppModule.controller('DemoController', ['TitleService', 'NotificationService', function(TitleService, notify) {
-    var vm = this;
-    vm.nombre = TitleService.Titulo();
-    vm.listado = [
-      { id: 1, nombre: 'Madrid'},
-      { id: 2, nombre: 'BARCELONA'},
-      { id: 3, nombre: 'sevilla'},
-      { id: 4, nombre: 'A coruñA'},
-    ];
-    vm.idProvincia = 2;
-    TitleService.Titulo('Soy una demo');
+cursoAppModule.controller('DemoController', ['TitleService', 'NotificationService', function (TitleService, notify) {
+  var vm = this;
+  vm.nombre = TitleService.Titulo();
+  vm.listado = [
+    { id: 1, nombre: 'Madrid' },
+    { id: 2, nombre: 'BARCELONA' },
+    { id: 3, nombre: 'sevilla' },
+    { id: 4, nombre: 'A coruñA' },
+  ];
+  vm.idProvincia = 2;
+  TitleService.Titulo('Soy una demo');
 
-    vm.resultado = null;
-    vm.visible = true;
-    vm.estetica = { error: false, importante: true, urgente: true };
+  vm.resultado = null;
+  vm.visible = true;
+  vm.estetica = { error: false, importante: true, urgente: true };
 
-    vm.saluda = function() {
-      vm.resultado = 'Hola ' + vm.nombre;
-      notify.add(vm.resultado);
-    }
-    vm.despide = function() {
-      vm.resultado = 'Adios ' + vm.nombre;
-    }
-    vm.di = function(algo) {
-      vm.resultado = 'Dice ' + algo;
-    }
+  vm.saluda = function () {
+    vm.resultado = 'Hola ' + vm.nombre;
+    notify.add(vm.resultado);
+  }
+  vm.despide = function () {
+    vm.resultado = 'Adios ' + vm.nombre;
+  }
+  vm.di = function (algo) {
+    vm.resultado = 'Dice ' + algo;
+  }
 
-    vm.cambia = function() {
-      vm.visible = !vm.visible;
-      vm.estetica.error = !vm.estetica.error;
-      vm.estetica.importante = !vm.estetica.importante;
-    }
+  vm.cambia = function () {
+    vm.visible = !vm.visible;
+    vm.estetica.error = !vm.estetica.error;
+    vm.estetica.importante = !vm.estetica.importante;
+  }
 
-    vm.calcula = function(a, b) { return a + b; }
+  vm.calcula = function (a, b) { return a + b; }
 
-    vm.add = function(provincia) {
-      var id = vm.listado.length == 0 ? 1 : (vm.listado[vm.listado.length - 1].id + 1);
-      vm.listado.push({id: id, nombre: provincia});
-      vm.idProvincia = id;
-    }
+  vm.add = function (provincia) {
+    var id = vm.listado.length == 0 ? 1 : (vm.listado[vm.listado.length - 1].id + 1);
+    vm.listado.push({ id: id, nombre: provincia });
+    vm.idProvincia = id;
+  }
 
-  }]);
+}]);
--- app/scripts/controllers/personas.js
+++ app/scripts/controllers/personas.js
@@ -1,16 +1,127 @@
-﻿angular.module('cursoApp').controller('PersonasController',
-  ['$log', '$scope', function ($log, $scope) {
-    var vm = this;
-    vm.listado = [];
-    vm.elemento = { id: 1, nombre: 'Pepito', apellidos: 'Grillo', edad: 99 };
-    vm.modo = 'edit';
-    vm.elemento = {  };
-    vm.modo = 'add';
+﻿angular.module('cursoApp').factory('PersonasDAO', ['$http', 'URLApi', function ($http, URLApi) {
+  var baseUrl = URLApi + 'personas';
+  var config = {};
+  return {
+    query: function () {
+      return $http.get(baseUrl, config);
+    },
+    get: function (id) {
+      return $http.get(baseUrl + '/' + id, config);
+    },
+    add: function (item) {
+      return $http.post(baseUrl, item, config);
+    },
+    change: function (id, item) {
+      return $http.put(baseUrl + '/' + id, item, config);
+    },
+    remove: function (id) {
+      return $http.delete(baseUrl + '/' + id, config);
+    }
+  };
+}]);
 
-    vm.cancelar = function() {
-      alert("Atras");
-    }
-    vm.enviar = function() {
-      alert(JSON.stringify(vm.elemento));
-    }
-  }]);
+angular.module('cursoApp').controller('PersonasController',
+  ['$log', '$scope', '$window', '$routeParams', '$location', 'PersonasDAO', 'NotificationService',
+    function ($log, $scope, $window, $routeParams, $location, dao, notify) {
+      var vm = this;
+      var idOriginal = null;
+      var urlListado = '/personas';
+
+      vm.listado = [];
+      vm.elemento = {};
+      vm.modo = 'list';
+
+      vm.list = function () {
+        dao.query().then(
+          function (resp) {
+            vm.listado = resp.data;
+            vm.modo = 'list';
+          },
+          function (err) {
+            notify.add(err.statusText);
+          }
+        );
+      }
+
+      vm.add = function () {
+        vm.elemento = {};
+        vm.modo = 'add';
+      }
+      vm.edit = function (id) {
+        dao.get(id).then(
+          function (resp) {
+            vm.elemento = resp.data;
+            vm.modo = 'edit';
+            idOriginal = id;
+          },
+          function (err) {
+            notify.add(err.statusText);
+          }
+        );
+      }
+      vm.view = function (id) {
+        dao.get(id).then(
+          function (resp) {
+            vm.elemento = resp.data;
+            vm.modo = 'view';
+          },
+          function (err) {
+            notify.add(err.statusText);
+          }
+        );
+      }
+      vm.delete = function (id) {
+        if (!$window.confirm('¿Seguro?')) return;
+        dao.remove(id).then(
+          function (resp) {
+            vm.list();
+          },
+          function (err) {
+            notify.add(err.statusText);
+          }
+        );
+
+      }
+      vm.cancelar = function () {
+        $location.url(urlListado);
+      }
+      vm.enviar = function () {
+        switch (vm.modo) {
+          case 'add':
+            dao.add(vm.elemento).then(
+              function (resp) {
+                vm.cancelar();
+              },
+              function (err) {
+                notify.add(err.statusText);
+              }
+            );
+            break;
+          case 'edit':
+            dao.change(idOriginal, vm.elemento).then(
+              function (resp) {
+                vm.cancelar();
+              },
+              function (err) {
+                notify.add(err.statusText);
+              }
+            );
+            break;
+          case 'view':
+            vm.cancelar();
+            break;
+        }
+      }
+
+      if($routeParams.id) {
+        if($location.url().endsWith('/edit'))
+          vm.edit($routeParams.id);
+        else
+          vm.view($routeParams.id);
+      } else {
+        if($location.url().endsWith('/add'))
+          vm.add();
+        else
+          vm.list();
+      }
+    }]);
